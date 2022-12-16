f53af1de063073f4a2e8864239a73b049257c671
Seb-M11 <sebastian.12345.ns.67@gmail.com>
2022-12-16 20:14:38 +0100

    elo

--- server.js
+++ server.js
@@ -2,9 +2,11 @@
 const express = require('express')
 const app = express()
 const port = 3000
-var mysql = require('mysql');
+var mysql = require('mysql')
+const bodyParser = require("body-parser")
 
 app.set('view-engine', 'ejs')
+var urlencodedParser = bodyParser.urlencoded({ extended: false })
 
 var con = mysql.createConnection({
     host: "containers-us-west-155.railway.app",
@@ -22,22 +24,72 @@ var con = mysql.createConnection({
 
 app.get('/', (req, res) => {
     res.render('index.ejs')
-    con.query("SELECT * FROM elo", function (err, result, fields) {
-        if (err) throw err;
-        console.log(result);
-        //console.log(result.RowDataPacket[0]);
-        console.log(result[0].id);
-      });
 })
 
-app.get('/plany', (req, res) => {
-    res.render('plany.ejs')
+app.get('/login', (req, res) => {
+    res.render('login.ejs')
 })
-
-
 app.get("/register", (req,res)=>{
     res.render("register.ejs")
 })
+app.get('/plany', (req, res) => {
+    res.render('plany.ejs')
+})
+app.get('/dieta', (req, res) => {
+    res.render('dieta.ejs')
+})
+app.get('/trening', (req, res) => {
+    res.render('trening.ejs')
+})
+
+app.post("/login", urlencodedParser, (req, res)=>{
+    var email = req.body.email
+    var pass = req.body.pass
+    con.connect(function(err) {
+    con.query(`SELECT * FROM users WHERE pass ="${pass}" AND email="${email}"`, function (err, result, fields) {
+    if (Object.keys(result).length > 0){
+      res.render("main")
+    }
+    else{ 
+      res.render("error-login", 
+      {
+          'email':email,
+          'pass':pass
+      })
+    }
+  })
+  })
+  })
+  
+  
+  app.post("/resister", urlencodedParser, (req,res)=>{
+      var name = req.body.name
+      var surname = req.body.surname
+      var email = req.body.email
+      var date = req.body.date
+      var pass = req.body.pass
+      var pass1 = req.body.pass1
+      if (pass == pass1){
+      con.connect(function(err) {
+          con.query(`INSERT INTO users(name, surname, email, birth_date, pass) VALUES ('${name}','${surname}','${email}','${date}','${pass}')`, function (err, result) {
+            if (err) throw err
+            res.render("success-signup")
+          })
+        })
+      }
+      else {
+        res.render("error-signup", 
+        {
+          'name':name, 
+          'surname':surname,
+          'email':email,
+          'date':date,
+          'pass':pass,
+          'pass1':pass1
+      })
+      }
+  })
+
 
 
 
