4cea992bbd0af8b8bb133ea7f858f27acc326641
Seb-M11 <sebastian.12345.ns.67@gmail.com>
2022-12-16 19:09:45 +0100

    elo

--- server.js
+++ server.js
@@ -1,8 +1,41 @@
-const express = require('express');
+
+const express = require('express')
 const app = express()
+const port = 3000
+var mysql = require('mysql');
 
 app.set('view-engine', 'ejs')
 
+var con = mysql.createConnection({
+    host: "containers-us-west-155.railway.app",
+    user: "root",
+    password: "6Ld3S1lwE0F6OhC0wOcw",
+    database: "railway",
+    port: 7792
+  });
+
+  con.connect(function(err) {
+    if (err) throw err;
+    console.log("Connected!");
+  });
+
+
 app.get('/', (req, res) => {
     res.render('index.ejs')
+    con.query("SELECT * FROM elo", function (err, result, fields) {
+        if (err) throw err;
+        console.log(result);
+        //console.log(result.RowDataPacket[0]);
+        console.log(result[0].id);
+      });
+})
+
+
+
+
+
+
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
 })
